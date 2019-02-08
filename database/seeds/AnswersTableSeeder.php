d4ed582ff4142eb4f377fab8589d8d2b405424b6
WWKING-PC\WWKING <wu0851202@gmail.com>
2019-02-09 02:35:09 +0800

    first commit

---
+++ database/seeds/AnswersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AnswersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('answers')->insert([
+            'answer' => '聽天由命ㄅ',
+        ]);
+    }
+}
