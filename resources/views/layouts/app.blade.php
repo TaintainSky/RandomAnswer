5e3dd0e8e245ba2df32edf9688ad0ce6cca4fca9
WWKING-GAMELAB\WWKING-GAMELAB <wu0851202@gmail.com>
2019-02-20 17:22:00 +0800

    新增 cool button

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <link rel="stylesheet" href="{{ asset('sass/app.css') }}">
+        <script src="{{ asset('js/manifest.js') }}"></script>
+        <script src="{{ asset('js/vendor.js') }}"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
+        @yield('script')
+        @yield('style')
+        <title>@yield('title','答案之網頁')</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+        <div class="layoutsContent">
+            @yield('content')
+        </div>
+    </body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,25 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/sass')
+    .options({
+        processCssUrls: false
+    })
+    .sourceMaps()
+    .browserSync({
+        port: 3002,
+        ui: {
+            port: 3003
+        },
+        host: 'localhost',
+        proxy: process.env.APP_URL,
+        files: [
+            '.env',
+            'route/**/*.php',
+            'app/**/*.php',
+            'resources/views/**/*.php',
+            'public/js/**/*.js',
+            'public/css/**/*.css'
+        ]
+    })
